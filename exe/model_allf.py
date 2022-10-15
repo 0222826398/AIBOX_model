4c2f84b2c977a0247123f9cb40f7970258e551e8
0222826398 <37056806+0222826398@users.noreply.github.com>
2022-10-15 15:09:02 +0800

    Updare

--- exe/model_allf.py
+++ exe/model_allf.py
@@ -5,34 +5,26 @@ import os
 from sklearn.model_selection import train_test_split
 import joblib
 
-df = pd.read_csv("exe/NewDataSet_134810_5m.csv")
-
-df.columns = df.columns.str.strip()
-print("original length of df:", len(df))
-df.replace([np.inf, -np.inf], np.nan, inplace=True)
-df.dropna(inplace=True)
-print("after droping null values, the length of df:", len(df))
-
-
-
-
-# df = df.drop(["src_ip" , "dst_ip", "src_port", "src_mac", "dst_mac", "protocol", "timestamp"], axis = 1)
-# df = df.drop(["Label"], axis = 1)
-
 std_scaler,mm_scaler,le = joblib.load("exe/new_model/1.5_std_mm_le_all_feature.save")
-
-X = df
-X = std_scaler.transform(X)
-print("after StandardScaler")
-print(X.shape)
-
-X = mm_scaler.transform(X)
-print("after MinMaxScaler")
-print(X.shape)
-
-
 model = joblib.load("exe/new_model/pkl/1.5_randomForest_all_feature_32.pkl")
-X_pred = model.predict(X)
+
+def data_preprocess(data):
+    data.columns = data.columns.str.strip()
+    data.replace([np.inf, -np.inf], np.nan, inplace=True)
+    data.dropna(inplace=True)
+    data_len = len(data)
+    data = data.drop(["src_ip" , "dst_ip", "src_port", "src_mac", "dst_mac", "protocol", "timestamp"], axis = 1)
+
+    data = mm_scaler.transform(std_scaler.transform(data))
+
+    return data
+
+
+df = pd.read_csv("exe/testflow.csv")
+
+data_flow = data_preprocess(df)
+
+X_pred = model.predict(data_flow)
 
 print(X_pred)
 print(X_pred.shape)
