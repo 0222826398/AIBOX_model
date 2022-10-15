6006b8b616f2f473e3554e18c4a242a56eaabfb0
0222826398 <37056806+0222826398@users.noreply.github.com>
2022-10-15 15:08:44 +0800

    Update

--- exe/model_20f.py
+++ exe/model_20f.py
@@ -5,35 +5,31 @@ import os
 from sklearn.model_selection import train_test_split
 import joblib
 
-df = pd.read_csv("exe/testflow.csv")
+std_scaler,mm_scaler,le = joblib.load("exe/new_model/1.5_std_mm_le_20_feature.save")
+model = joblib.load("exe/new_model/pkl/1.5_randomForest_20_feature_32.pkl")
 
-df.columns = df.columns.str.strip()
-print("original length of df:", len(df))
-df.replace([np.inf, -np.inf], np.nan, inplace=True)
-df.dropna(inplace=True)
-print("after droping null values, the length of df:", len(df))
+def data_preprocess(data):
+    data.columns = data.columns.str.strip()
+    data.replace([np.inf, -np.inf], np.nan, inplace=True)
+    data.dropna(inplace=True)
+    data_len = len(data)
 
-
-
-
-df = df[["flow_duration" , "bwd_pkt_len_max", "bwd_pkt_len_min", "bwd_pkt_len_mean", "bwd_pkt_len_std", "flow_iat_std", "flow_iat_max", "fwd_iat_tot", "fwd_iat_std", "fwd_iat_max", 
+    data = data[["flow_duration" , "bwd_pkt_len_max", "bwd_pkt_len_min", "bwd_pkt_len_mean", "bwd_pkt_len_std", "flow_iat_std", "flow_iat_max", "fwd_iat_tot", "fwd_iat_std", "fwd_iat_max", 
          "pkt_len_min", "pkt_len_max", "pkt_len_mean", "pkt_len_std", "pkt_len_var", "pkt_size_avg", "bwd_seg_size_avg", "idle_mean", "idle_max", "idle_min"]]
 
+    data = mm_scaler.transform(std_scaler.transform(data))
 
-std_scaler,mm_scaler,le = joblib.load("exe/new_model/1.5_std_mm_le_20_feature.save")
-
-X = df
-X = std_scaler.transform(X)
-print("after StandardScaler")
-print(X.shape)
-
-X = mm_scaler.transform(X)
-print("after MinMaxScaler")
-print(X.shape)
+    return data
 
 
-model = joblib.load("exe/new_model/pkl/1.5_randomForest_20_feature_32.pkl")
-X_pred = model.predict(X)
+    
+df = pd.read_csv("exe/testflow.csv")
+
+data_flow = data_preprocess(df)
+
+
+
+X_pred = model.predict(data_flow)
 
 print(X_pred)
 print(X_pred.shape)
