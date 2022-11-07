bdce3556f555842ca2b48777e5f9e67adc4f0fbd
0222826398 <37056806+0222826398@users.noreply.github.com>
2022-11-07 15:05:34 +0800

    Add files via upload

--- aibox-integrated/cic.py
+++ aibox-integrated/cic.py
@@ -2,77 +2,120 @@ import subprocess
 import signal
 import os
 import time
+import joblib
+import numpy as np
+import pandas as pd
+import os
+import threading as td
 
-def cicflowmeter(start):
+std_scaler, mm_scaler, le = joblib.load("exe/new_model/25679_std_mm_le_all_feature.save")
+model = joblib.load("exe/new_model/pkl/25679_randomForest_all_feature_32.pkl")
+Flag = True
+C = 0
+
+def cicflowmeter(start,C = 0):
     global pid
+    print('eth0')
     if start:     
         p = subprocess.Popen(["cicflowmeter","-i",
-                            "lo","-c",
-                            'flows.csv']) # Call subprocess
+                            "eth0","-c",
+                            'flow/flows' + str(C) + '.csv']) # Call subprocess
         pid = p.pid
     elif not start:   
         os.kill(pid, signal.SIGINT)
-        # os.kill(pid, signal.SIGKILL)
+
+    return None
 
 
-cicflowmeter(True)
-print("cicflowmeter started")
-time.sleep(30)
-print("cicflowmeter stopped")
-cicflowmeter(False)
+def data_preprocess(data):
+    data.columns = data.columns.str.strip()
+    data.replace([np.inf, -np.inf], np.nan, inplace=True)
+    data.dropna(inplace=True)
+    data_len = len(data)
+    data = data.drop(["src_ip" , "dst_ip", "src_port", "src_mac", "dst_mac", "protocol", "timestamp"], axis = 1)
+    #      data = data[["flow_duration" , "bwd_pkt_len_max", "bwd_pkt_len_min", "bwd_pkt_len_mean", "bwd_pkt_len_std", "flow_iat_std", "flow_iat_max", "fwd_iat_tot", "fwd_iat_std", "fwd_iat_max", 
+    #      "pkt_len_min", "pkt_len_max", "pkt_len_mean", "pkt_len_std", "pkt_len_var", "pkt_size_avg", "bwd_seg_size_avg", "idle_mean", "idle_max", "idle_min"]]
+
+    data = mm_scaler.transform(std_scaler.transform(data.values))
+
+    return data, data_len
 
 
-print("model start!!!!!")
-###### 資料讀取 實際測試時此區改為讀取攔截之封包資料
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import os
-testdata = "flows.csv"
-df = pd.read_csv(testdata)
+def cic_main():
+    global Flag
+    global C
 
-df.columns = df.columns.str.strip()
-print("original length of df:", len(df))
-df.replace([np.inf, -np.inf], np.nan, inplace=True)
-df.dropna(inplace=True)
-print("after droping null values, the length of df:", len(df))
+    while(True):
+        cicflowmeter(True, C)
+        print("cicflowmeter started")
+        time.sleep(5)
+        Flag = False
+        print("cicflowmeter stopped")
+        cicflowmeter(False)
+        # time.sleep(0.5)
+        C = C + 1
 
-df = df.drop(["src_ip" , "dst_ip", "src_port", "src_mac", "dst_mac", "protocol", "timestamp"], axis = 1)
-
-# data split and preprocess
-from sklearn.model_selection import train_test_split
-import joblib
-
-std_scaler,mm_scaler,le = joblib.load("std_mm_le_new.save")
-
-X = df
-
-X = std_scaler.transform(X)
-
-print("after StandardScaler")
-print(X.shape)
-
-X = mm_scaler.transform(X)
-
-print("after MinMaxScaler")
-print(X.shape)
-
-#標籤編碼 可不跑
-# y_train = le.transform(y_train)
-# y_test = le.transform(y_test)
+    return None
 
 
-###### Random Forest
-import joblib
+def model_main():
+    global Flag
+    global C
 
-rng = np.random.RandomState(42)
+    cnt = 0
+    while(True):
+        while(not Flag):
+            print("////flow predict////")
+            Flag = True
+            time.sleep(1)
+            testdata = "flow/flows" + str(C-1) + ".csv"
+            print(testdata)
 
-model = joblib.load("new_randomForest_32.pkl")
-X_pred = model.predict(X)
+            try:
+                df = pd.read_csv(testdata)
 
-print(X_pred)
-print(X_pred.shape)
+            except pd.errors.EmptyDataError: # or BaseException
+                f_len, P_A, P_L, f_pred = 0, None, None, None
+            
+            else:
+                data_flow, f_len = data_preprocess(df)
 
-print("percentage of Anomaly:", (list(X_pred).count(1)/X_pred.shape[0])*100)
-print("percentage of Legit:",(list(X_pred).count(0)/X_pred.shape[0])*100)
+                f_pred = model.predict(data_flow)
+                P_A = (list(f_pred).count(1)/f_pred.shape[0])*100
+                P_L = (list(f_pred).count(0)/f_pred.shape[0])*100
 
-print("model end!!!!!")
+            finally:
+                '''Test for dataset'''
+                if (f_len and f_pred.any()) == 1:
+                   print("----------------------------------------------------------")
+                   print("     Time                    : \t", time.asctime(time.localtime(time.time())))
+                   print("     /////////Alert///////// : Anomaly traffic detected \t")
+                   print("     flow file               : \t", testdata)
+                   print("----------------------------------------------------------")
+                   '''move error data to Abnormal file'''
+                   desdata = "Abnormal/" + time.asctime(time.localtime(time.time())) + ".csv"
+                   os.replace(testdata, desdata)
+                   break
+
+                print(f_pred)
+                print("----------------------------------------------------------")
+                print("     Time                    : \t", time.asctime(time.localtime(time.time())))
+                print("     Amount of flows         : \t", f_len)
+                print("     Percentage of Anomaly   : \t", P_A)
+                print("     Percentage of Legit     : \t", P_L)
+                print("----------------------------------------------------------")
+
+                os.remove(testdata)
+                cnt = cnt + 1
+                print("cnt = ",cnt)
+        
+    return None
+
+t_cic = td.Thread(target = cic_main)
+t_model = td.Thread(target = model_main)
+
+t_cic.start()
+t_model.start()
+
+t_cic.join()
+t_model.join()
